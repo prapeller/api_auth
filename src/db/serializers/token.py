9058eb583574fc92f99b48b21b91cd494924a1de
prapeller <prapeller@mail.ru>
2023-06-26 11:01:48 +0400

    chore: correct token_schema with .from_jwt method

--- src/db/serializers/token.py
+++ src/db/serializers/token.py
@@ -1,9 +1,10 @@
 import datetime as dt
+import typing
 
 import pydantic as pd
 from pydantic.main import object_setattr
 
-from core.enums import TokenTypesEnum, PermissionsNamesEnum
+from core.enums import TokenTypesEnum, PermissionsNamesEnum, OAuthTypesEnum
 from core.security import get_token_data
 
 
@@ -20,15 +21,20 @@ class TokenCreateSchema(pd.BaseModel):
 
     exp: dt.datetime  # expiration time
 
+    oauth_type: OAuthTypesEnum
+    oauth_token: str
+
 
 class TokenReadSchema(TokenCreateSchema):
     jti: str  # token_id
     iat: dt.datetime  # issued_at time
 
     @classmethod
-    def from_jwt(cls, encoded_jwt: str) -> 'TokenReadSchema':
+    def from_jwt(cls, encoded_jwt: str) -> typing.Union['TokenReadSchema', None]:
         m = cls.__new__(cls)
-        decoded_jwt: dict = get_token_data(encoded_jwt)
+        decoded_jwt: dict | None = get_token_data(encoded_jwt)
+        if decoded_jwt is None:
+            return None
         object_setattr(m, '__dict__', decoded_jwt)
         object_setattr(m, '__fields_set__', cls.__fields_set__)
         m._init_private_attributes()
