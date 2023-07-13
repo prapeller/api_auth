a9ce773c843bd57624ef5eeb27020b7e3083870d
prapeller <prapeller@mail.ru>
2023-07-13 16:49:46 +0400

    chore: refactor async initial scripts

--- api_auth/scripts/create_permissions_roles.py
+++ api_auth/scripts/create_permissions_roles.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from core.enums import RolesNamesEnum, PermissionsNamesEnum
 from db import SessionLocalAsync, init_models
 from db.models.permission import PermissionModel
@@ -10,19 +12,17 @@ async def create_permissions_all():
     permission_ser_list = []
     for perm_name in PermissionsNamesEnum:
         permission_ser_list.append(PermissionCreateSerializer(name=perm_name))
-    with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
+    async with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
         await repo.get_or_create_many(PermissionModel, permission_ser_list)
 
 
 async def create_roles_all():
-    await create_permissions_all()
-
-    with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
+    async with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
         for role_name in RolesNamesEnum:
             if role_name == RolesNamesEnum.superuser:
                 is_created, superuser_role = await repo.get_or_create_by_name(RoleModel, role_name)
                 is_created, all_of_all_perm = await repo.get_or_create_by_name(PermissionModel,
-                                                                         PermissionsNamesEnum.all_of_all)
+                                                                               PermissionsNamesEnum.all_of_all)
                 superuser_role.permissions.append(all_of_all_perm)
 
             if role_name == RolesNamesEnum.staff:
@@ -46,6 +46,11 @@ async def create_roles_all():
             await repo.session.commit()
 
 
+async def create_roles_and_permissions():
+    await create_permissions_all()
+    await create_roles_all()
+
+
 if __name__ == '__main__':
     init_models()
-    await create_roles_all()
+    asyncio.run(create_roles_and_permissions())
--- api_auth/scripts/create_superuser.py
+++ api_auth/scripts/create_superuser.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pydantic as pd
 
 from core.enums import RolesNamesEnum
@@ -5,30 +7,29 @@ from db import init_models, SessionLocalAsync
 from db.models.role import RoleModel
 from db.repository import SqlAlchemyRepositoryAsync
 from db.serializers.user import UserCreateSerializer
-from scripts.create_permissions_roles import create_roles_all
 
 
-def create_superuser():
+async def create_superuser():
     init_models()
 
-    await create_roles_all()
     while True:
         name = str(input('enter superuser name >>> '))
         email = str(input('enter superuser email >>> '))
         password = str(input('enter superuser password >>> '))
         try:
             superuser_ser = UserCreateSerializer(name=name, email=email, password=password, is_active=True)
-            with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
-                superuser = await superuser_ser.create(repo)
+            async with SqlAlchemyRepositoryAsync(SessionLocalAsync()) as repo:
+                superuser = await repo.create_user(superuser_ser)
                 is_created, superuser_role = await repo.get_or_create_by_name(RoleModel, RolesNamesEnum.superuser)
                 superuser.roles.append(superuser_role)
                 await repo.session.commit()
                 await repo.session.refresh(superuser)
                 print(f'Successfully created {superuser=:}')
                 break
+
         except pd.ValidationError as e:
             print(e)
 
 
 if __name__ == '__main__':
-    create_superuser()
+    asyncio.run(create_superuser())
