62abf4580fc4cfe37ba02c8d260b56c997271224
prapeller <prapeller@mail.ru>
2023-08-17 23:28:37 +0300

    chore: refactor helpers_users for tests, add x-request headers to tests

--- api_auth/tests/functional/plugins/aiohttp_plugin.py
+++ api_auth/tests/functional/plugins/aiohttp_plugin.py
@@ -11,18 +11,21 @@ async def body_status():
     async def inner(url: str, method: MethodsEnum = MethodsEnum.get, params: dict = None, data: dict = None,
                     headers=None):
 
+        if headers is None:
+            headers = {
+                'X-Request-Id': 'test',
+                'Content-Type': 'application/json',
+            }
+            data = json.dumps(data)
+
         async with aiohttp.ClientSession() as session:
             if method == MethodsEnum.get:
-                async with session.get(url, params=params) as response:
+                async with session.get(url, headers=headers, params=params) as response:
                     body = await response.json()
                     status = response.status
                     return body, status
-
             elif method == MethodsEnum.post:
-                if headers is None:
-                    headers = {'Content-Type': 'application/json'}
-                    data = json.dumps(data)
-                async with session.post(url, data=data, headers=headers) as response:
+                async with session.post(url, headers=headers, data=data) as response:
                     body = await response.json()
                     status = response.status
                     return body, status
